1e48019c4f45adb61999c8ecbcbc6a2798f97c0d
lepetitpaco <daviphone121@gmail.com>
2024-02-22 21:17:49 +0000

    :sparkles: Improve front

--- index.php
+++ index.php
@@ -3,43 +3,37 @@
 // Start the session
 session_start();
 
+// Get the root directory of the website
+$root_dir = $_SERVER['DOCUMENT_ROOT'] . '/';
+
+// Get the root directory of the website
+$baseUrl = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://" . $_SERVER['HTTP_HOST'] . '/';
+
+// Get a list of all directories in the root directory
+$dirs = array_filter(glob($root_dir . '*'), 'is_dir');
+$forbidden_folders = array('vendor');
 ?>
+
 <?php include('menu.php'); ?>
 
-
+<!DOCTYPE html>
+<html lang="fr">
 
 <!DOCTYPE html>
-<html>
+<html lang="fr">
 
 <head>
-    <title>Useless shit you won't need anyways but you got access to it so you might as well try and suffer a bit. Well
-        fuck I'm tired of this life I wish I could code a button that ends me on the spot but I can't even do that, fml,
-        I'm going to watch animes.</title>
-</head>
-
-<body>
-
-    <div class="container">
-        <div class="jumbotron">
-            <h1 class="display-4">Welcome to the Toolbox</h1>
-            <p class="lead">Here, you probably won't find what you need right now, but I offer you other stuff you won't
-                need. My collection of tools is vast and varied, and I am sure you'll find something that you don't
-                need. <br>My tools are designed to be a pain in the ass and complicated to use, even if you need them.
-            </p>
-            <hr class="my-4">
-            <p>Explore my subfolders to find the tools that you don't need. We hope you find my website useless.</p>
-        </div>
-    </div>
-
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Boîte à Outils - Le Petit Paco</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-
     <style>
         .jumbotron {
             background-color: #f8f9fa;
             color: #212529;
             border-radius: 0;
-            margin-bottom: 0;
             text-align: justify;
+            margin-bottom: 20px;
         }
 
         .container {
@@ -47,12 +41,68 @@ session_start();
             margin: 0 auto;
             padding: 0 15px;
         }
+
+        .card {
+            cursor: pointer;
+            transition: transform 0.3s;
+        }
+
+        .card:hover {
+            transform: translateY(-5px);
+            text-decoration: none;
+        }
+
+        .card-body {
+            padding: 1.25rem;
+            text-align: center;
+        }
+
+        .card-title {
+            margin-bottom: 0.75rem;
+            text-transform: uppercase;
+        }
     </style>
+</head>
 
+<body>
 
-</body>
+    <div class="container">
+        <div class="jumbotron">
+            <h1 class="display-4">Bienvenue dans la Boîte à Outils</h1>
+            <p class="lead">Ici, vous ne trouverez probablement pas ce dont vous avez besoin en ce moment, mais je vous
+                propose d'autres choses dont vous n'aurez pas besoin. Ma collection d'outils est vaste et variée, et je
+                suis
+                sûr que vous trouverez quelque chose dont vous n'avez pas besoin. <br> Mes outils sont conçus pour être
+                une
+                vraie prise de tête et compliqués à utiliser, même si vous en avez besoin.</p>
+            <hr class="my-4">
+            <p>Explorez mes sous-dossiers pour trouver les outils dont vous n'avez pas besoin. Nous espérons que vous
+                trouverez mon site inutile.</p>
+        </div>
 
-</html>
+        <div class="row">
+            <?php
+            // Directories to exclude from the loop
+            $excluded_dirs = ['anilist', 'vendor'];
+
+            foreach ($dirs as $dir):
+                $dir_name = basename($dir);
+                if (in_array($dir_name, $excluded_dirs))
+                    continue;
+                ?>
+                <div class="col-md-4">
+                    <a href="<?php echo $baseUrl . $dir_name; ?>" class="card mb-4 shadow-sm">
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                <?php echo ucfirst($dir_name); ?>
+                            </h5>
+                        </div>
+                    </a>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
+
 </body>
 
 </html>
